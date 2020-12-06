9313ccaaaf3d2282f5db42764b3d11315e495e63
Victor <ageneralov902@gmail.com>
2020-12-06 14:56:55 +0400

    i add a getChildren.js script

---
+++ katas/getChildren.js
@@ -0,0 +1,29 @@
+const users = [
+  {
+    name: 'Tirion',
+    children: [
+      { name: 'Mira', birthday: '1983-03-23' },
+    ],
+  },
+  { name: 'Bronn', children: [] },
+  {
+    name: 'Sam',
+    children: [
+      { name: 'Aria', birthday: '2012-11-03' },
+      { name: 'Keit', birthday: '1933-05-14' },
+    ],
+  },
+  {
+    name: 'Rob',
+    children: [
+      { name: 'Tisha', birthday: '2012-11-03' },
+    ],
+  },
+];
+
+function getChildren(arr) {
+  const children = arr.map((user) => { return user.children })
+  return children.flat(2);
+}
+
+console.log(getChildren(users));
