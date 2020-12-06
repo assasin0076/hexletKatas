fe39194d37960365c1265e91a0d724c114afbc51
Victor <ageneralov902@gmail.com>
2020-12-06 09:13:59 +0400

    added SortArr.js

--- script.js
+++ script.js
@@ -1,31 +1,18 @@
-function getMenCountByYear(people) {
-  const menCountByYear = {}
-  
-  for (const person of people) {
-    if (person.gender === 'male') {
-      const birthYear = person.birthday.slice(0, 4) + '';
-      if (!menCountByYear.hasOwnProperty(birthYear)) {
+import _ from 'lodash';
 
-        menCountByYear[birthYear] = 1;
-      } else {
-        menCountByYear[birthYear] += 1;
-      }
-    }
-  }
-  return menCountByYear;
+const sort = (users, num = 1) => {
+  const filtered = _.sortBy(users, ({birthday}) => Date.parse(birthday));
+  return _.slice(filtered, 0, num);
 }
 
-const users = [
-  { name: 'Bronn', gender: 'male', birthday: '1973-03-23' },
-  { name: 'Reigar', gender: 'male', birthday: '1973-11-03' },
-  { name: 'Eiegon', gender: 'male', birthday: '1963-11-03' },
-  { name: 'Sansa', gender: 'female', birthday: '2012-11-03' },
-  { name: 'Jon', gender: 'male', birthday: '1980-11-03' },
-  { name: 'Robb', gender: 'male', birthday: '1980-05-14' },
-  { name: 'Tisha', gender: 'female', birthday: '2012-11-03' },
-  { name: 'Rick', gender: 'male', birthday: '2012-11-03' },
-  { name: 'Joffrey', gender: 'male', birthday: '1999-11-03' },
-  { name: 'Edd', gender: 'male', birthday: '1973-11-03' },
-];
 
-console.log(getMenCountByYear(users));
+const users = [
+  { name: 'Tirion', birthday: 'Nov 19, 1988' },
+  { name: 'Sam', birthday: 'Nov 22, 1999' },
+  { name: 'Rob', birthday: 'Jan 11, 1975' },
+  { name: 'Sansa', birthday: 'Mar 20, 2001' },
+  { name: 'Tisha', birthday: 'Feb 27, 1992' },
+  { name: 'Chris', birthday: 'Dec 25, 1995' },
+];
+const struc = sort(users, 3);
+console.log(struc[0]);
